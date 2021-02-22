defd9b531f8b519aaa785bd4835ba479d9526d04
ajagena <aja5442@rit.edu>
2021-02-22 17:35:40 -0500

    Created basic hardcoded layout of dashboard

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
+
 import './App.css';
+import { Card, Button, Container } from 'react-bootstrap';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          <h1>Job Application Dashboard</h1>
       </header>
+      <div className="App-body">
+          <Container className="main">
+              <div className="untagged">
+                  <h3>Untagged</h3>
+                  {/* placeholder */}
+                  <Card>
+                      <Card.Body>
+                          <Card.Title>NYTimes</Card.Title>
+                      </Card.Body>
+                  </Card>
+
+                  {/* placeholder */}
+                  <Card>
+                      <Card.Body>
+                          <Card.Title>GameStop</Card.Title>
+                      </Card.Body>
+                  </Card>
+              </div>
+              {/* <div className="newCol">
+                <h3>Preferred</h3>
+                <Card>
+                    <Card.Body>
+                        <Card.Title>Datto</Card.Title>
+                    </Card.Body>
+                </Card>
+              </div> */}
+          </Container>
+          <div className="options">
+              <Button variant="primary" className="btn-primary">+ New Application</Button> {' '}
+              <h2>Tags</h2>
+              <div>
+                  <p>#preferred</p> {/* placeholder */}
+                  <p>#backups</p>   {/* placeholder */}
+              </div>
+          </div>
+      </div>
     </div>
   );
 }
