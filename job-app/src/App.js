6576a0de0700c2d1222b589ea698e5982cf46968
Paul Gildehaus <pgildehaus@morevang.com>
2021-02-26 10:59:35 -0500

    Struggling

--- job-app/src/App.js
+++ job-app/src/App.js
@@ -1,22 +1,40 @@
 import React from "react";
 import logo from './logo.svg';
 import './App.css';
+const axios = require('axios');
 
 function App() {
-  const [data, setData] = React.useState(null);
+  //const [data, setData] = React.useState(null);
+  const data = "";
 
+/*
   React.useEffect(() => {
-    fetch("/api")
+    fetch("/applications")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data[0].Salary));
   }, []);
+*/
+
+  // Make a request for a user with a given ID
+  axios.get('/applications')
+    .then(function (response) {
+      // handle success
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    })
+    .then(function () {
+      // always executed
+    });
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          {!data ? "Loading..." : data}
+          {data}
         </p>
       </header>
     </div>
--- server/index.js
+++ server/index.js
@@ -1,12 +1,42 @@
 const express = require("express");
-
 const PORT = 3001;
-
 const app = express();
+// Node File Stream object
+const fs = require('fs');
 
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!" });
+// Dictionary declaration for application items
+applications = [];
+
+async function ReadJson(jsonID) {
+    const data = await fs.promises.readFile('./server/storage/' + jsonID, 'utf8');
+    jsonData = JSON.parse(data);
+    return jsonData;
+}
+
+ReadJson('exampleJson.json').then(function(result) {
+    app.get("/applications", (req, res) => {
+      res.json(result);
+    });
 });
+
+//applications[1]["Salary"] = "$55/hour";
+//console.log(applications);
+//WriteJson('exampleJson.json');
+
+function WriteJson(jsonID) {
+    jsonString = JSON.stringify(applications);
+    fs.writeFile('./server/storage/' + jsonID, jsonString, err => {
+        if (err) {
+            console.log('Error writing applications to file: ' + err);
+        } else {
+            console.log('Applications Saved to ${jsonID}.json');
+        }
+    })
+}
+
+
+
+app.get("/")
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
